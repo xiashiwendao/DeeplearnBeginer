f0630dcf08df0a591646cc23ae817fff5640a7ec
python_gay <xiashiwendao@163.com>
2020-02-17 14:18:17 +0800

    build 3-layer network

---
+++ neuro_network/neualnet.py
@@ -0,0 +1,37 @@
+import numpy as np
+import sys, os
+sys.path.append(os.getcwd())
+from common.functions import sigmoid, softmax
+
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+
+    network['b3'] = np.array([0.1, 0.2])
+    return network
+
+def identity_function(x):
+    return x
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    # 输出层看问题而定，如果是回归问题恒等函数就可了，如果是分类问题需要使用sigmoid/softmax
+    y = identity_function(a3)
+
+    return y
+
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y) # [ 0.31682708 0.69627909]
